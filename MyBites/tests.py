e32000536a06e2bb349e822908b3f010fb6011c7
DanielAngger <angger.7d@gmail.com>
2024-10-27 20:31:42 +0700

    MyBites

--- MyBites/tests.py
+++ MyBites/tests.py
@@ -9,27 +9,23 @@ class MyBitesTests(TestCase):
         self.user = User.objects.create_user(username='testuser', password='testpassword')
 
         self.product = Product.objects.create(
-            nama_toko_lokasi='Test Store Location',
-            nama_product='Test Product',
-            harga=10000,
-            deskripsi='Test Description',
-            kalori=200,
-            tag_kalori='rendah',
-            tag_vegan='vegan',
-            tag_gula='rendah'
+            store='QITA_MART',  
+            name='Test Product',
+            price=10000,
+            description='Test Description',
+            calories=200,
+            calorie_tag='LOW',  
+            vegan_tag='VEGAN', 
+            sugar_tag='LOW'     
         )
 
         self.client.login(username='testuser', password='testpassword')
+
 
     def test_add_to_wishlist_logged_in(self):
         response = self.client.post(reverse('MyBites:add_to_wishlist', args=[self.product.id]))
         self.assertEqual(response.status_code, 302)  # Redirect after adding
         self.assertTrue(MyBites.objects.filter(user=self.user, product=self.product).exists())
-
-    def test_add_to_wishlist_not_logged_in(self):
-        self.client.logout()
-        response = self.client.post(reverse('MyBites:add_to_wishlist', args=[self.product.id]))
-        self.assertRedirects(response, f'/accounts/login/?next=/MyBites/add_to_wishlist/{self.product.id}/')
 
     def test_view_wishlist_logged_in(self):
         MyBites.objects.create(user=self.user, product=self.product)
@@ -46,7 +42,7 @@ class MyBitesTests(TestCase):
     def test_show_main(self):
         response = self.client.get(reverse('main:show_main'))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'main/main.html')
+        self.assertTemplateUsed(response, 'main.html')
 
     def test_show_json(self):
         response = self.client.get(reverse('MyBites:show_json'))
