ca7486a146425f4cd3fe346e3977c0b0c809a660
brjaeger <bryanmitch48@gmail.com>
2024-10-27 20:47:21 +0700

    menyelesaikan designn

--- bite_tracker/settings.py
+++ bite_tracker/settings.py
@@ -40,10 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'editbites',
     'artibites',
     'main',
     'MyBites',
+    'TrackerBites',
+    'editbites',
+    'sharebites',
 ]
 
 MIDDLEWARE = [
