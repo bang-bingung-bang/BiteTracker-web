c3fc875f925e3dc3a892555363a54849d64bd37d
brjaeger <bryanmitch48@gmail.com>
2024-10-28 00:25:43 +0700

    membuat unittest

--- bite_tracker/settings.py
+++ bite_tracker/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'TrackerBites',
     'editbites',
     'sharebites',
+    'artibites'
 ]
 
 
--- bite_tracker/urls.py
+++ bite_tracker/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('', include('TrackerBites.urls')),
+    path('artibites/', include('artibites.urls', namespace='artibites')),
     path('mybites/', include('MyBites.urls', namespace='MyBites')),
     path('sharebites/', include('sharebites.urls')),
     path('productbites/', include('editbites.urls', namespace='editbites')),
