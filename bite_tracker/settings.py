d966991e2a81a15559eb1671f1abe05913b11f20
FaizAP412 <faiz.pribadi@gmail.com>
2024-12-28 16:06:24 +0700

    finish

--- bite_tracker/settings.py
+++ bite_tracker/settings.py
@@ -159,13 +159,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-DEBUG = True
-# if DEBUG:
-#     STATICFILES_DIRS = [
-#         BASE_DIR / 'static'
-#     ]
-# else:
-#     STATIC_ROOT = BASE_DIR / 'static'
+# DEBUG = True
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static'
+    ]
+else:
+    STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
